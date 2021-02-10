5f8ae92fe662061957640a000f75aa7ddf883210
LaneFiddy <lanefisherr@gmail.com>
2021-02-09 19:54:47 -0600

    the command group might be closer to working, but i'm not sure.

--- src/commands/releaseshoot.py
+++ src/commands/releaseshoot.py
@@ -2,18 +2,17 @@
 '''Drive differentially with an Xbox controller.'''
 
 from wpilib.command import CommandGroup
-from commands.block import Block
-from commands.shoot import Shoot
-from commands.stopshooting import StopShooting
-from commands.unblock import Unblock
+import commands
 from wpilib.command import WaitCommand
 
 class ReleaseShoot(CommandGroup):
     def __init(self, robot):
-        self.addSequential(Shoot(robot))
+        super().__init__()
+
+        self.addSequential(commands.Shoot(robot))
         self.addSequential(WaitCommand(timeout=0.3))
-        self.addSequential(Unblock(robot))
+        self.addSequential(commands.Unblock(robot))
         self.addSequential(WaitCommand(timeout=5))
         #TODO: figure out how long the shooter needs to run in order to empty
-        self.addSequential(StopShooting(robot))
-        self.addParallel(Block(robot))
+        self.addSequential(commands.StopShooting(robot))
+        self.addParallel(commands.Block(robot))
