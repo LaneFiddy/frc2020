4a31101b77e2100012535e68259483f6759076f6
4975jonah <jonah.jepsen@gmail.com>
2021-06-17 14:21:09 -0500

    auto_backup_shoot should work. If it doesn't, none of the other autonomous routines will.

---
+++ src/auto_backup_shoot.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from wpilib.command import CommandGroup
+from wpilib.command import WaitCommand
+from .shoot import shoot
+from .unblock import Unblock
+from .drive_reverse import DriveReverse
+
+class AutoBackupShoot(CommandGroup):
+    def __init__(self, robot):
+        super().__init__()
+        self.robot = robot
+
+        self.addSequential(DriveReverse(robot), 2.0)
+        self.addSequential(Shoot(robot), 2.0)
+
